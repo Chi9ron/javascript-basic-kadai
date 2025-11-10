2b7f37f78fbd5bf3657d68ec1baaeb1715bc4596
Chi9ron <86711177+Chi9ron@users.noreply.github.com>
2025-11-10 22:46:23 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let n
+let num = Math.floor(Math.random() * n) + 1
+console.log(num);
+switch (num) {
+    case num % 3 == 0:
+        console.log('3の倍数です');
+        break;
+    case num % 5 == 0:
+        console.log('5の倍数です');
+        break;
+    case num % 3 == 0 || num % 5 == 0:
+        console.log('3と5の倍数です');
+        break;
+    default:
+        console.log(num);
+    break;
+}
